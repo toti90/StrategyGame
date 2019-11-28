36711b88c36b3e4aea7f7706279a6b550af00e10
toti90 <tothtomi7@gmail.com>
2019-11-28 12:11:33 +0100

    add AddPoint to Unit table

--- backend/StrategyGame.Dal/Migrations/20191128110836_add AddPoin to Units.Designer.cs
+++ backend/StrategyGame.Dal/Migrations/20191128110836_add AddPoin to Units.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StrategyGame.Dal;
 
 namespace StrategyGame.Dal.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191128110836_add AddPoin to Units")]
+    partial class addAddPointoUnits
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/StrategyGame.Dal/Migrations/20191128110836_add AddPoin to Units.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StrategyGame.Dal.Migrations
+{
+    public partial class addAddPointoUnits : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AddPoint",
+                table: "Units",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddPoint",
+                table: "Units");
+        }
+    }
+}
